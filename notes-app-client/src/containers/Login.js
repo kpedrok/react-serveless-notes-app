f2529b6efc9da93d1cccf7ee6d2455dd213add34
unknown <pedrokvb@gmail.com>
2019-08-19 14:53:42 -0300

    loggin

--- notes-app-client/src/containers/Login.js
+++ notes-app-client/src/containers/Login.js
@@ -27,12 +27,15 @@ export default class Login extends Component {
   handleSubmit = async event => {
     event.preventDefault();
 
+    this.setState({ isLoading: true });
+
     try {
       await Auth.signIn(this.state.email, this.state.password);
       this.props.userHasAuthenticated(true);
       this.props.history.push("/");
     } catch (e) {
       alert(e.message);
+      this.setState({ isLoading: false });
     }
   };
 
